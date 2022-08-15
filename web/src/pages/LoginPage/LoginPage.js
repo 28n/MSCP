90d23afc5b74eef0d0e0fd70b2d1df1215fce18a
28n <28nh04@gmail.com>
2022-08-15 12:27:08 +0200

    Advanced Devmode first commit

--- web/src/pages/LoginPage/LoginPage.js
+++ web/src/pages/LoginPage/LoginPage.js
@@ -15,7 +15,7 @@ import { useEffect, useState } from 'react'
 
 const LoginPage = () => {
   const { isAuthenticated, logIn } = useAuth()
-  const [devmode, setdevmode] = useState(false)
+  const [devmode, setdevmode] = useState(0)
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -27,8 +27,12 @@ const LoginPage = () => {
   useEffect(() => {
     usernameRef.current.focus()
     let x = localStorage.getItem('devmode')
+    console.log(x)
     if (x === '45lCEODB4a0LBIAD') {
-      setdevmode(true)
+      setdevmode(1)
+    }
+    if (x === 'true' || x === true) {
+      setdevmode(99)
     }
   }, [])
 
@@ -51,86 +55,88 @@ const LoginPage = () => {
       <main className="rw-main">
         <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
         <div className="rw-scaffold rw-login-container">
-          <div className="rw-segment">
-            <header className="rw-segment-header">
-              <h2 className="rw-heading rw-heading-secondary">
-                MSCP | Team-ELAN.de
-              </h2>
-            </header>
+          {devmode !== 99 ? (
+            <div className="rw-segment">
+              <header className="rw-segment-header">
+                <h2 className="rw-heading rw-heading-secondary">
+                  MSCP | Team-ELAN.de
+                </h2>
+              </header>
 
-            <div className="rw-segment-main">
-              <div className="rw-form-wrapper">
-                <Form onSubmit={onSubmit} className="rw-form-wrapper">
-                  <Label
-                    name="username"
-                    className="rw-label"
-                    errorClassName="rw-label rw-label-error"
-                  >
-                    Benutzertag
-                  </Label>
-                  <TextField
-                    name="username"
-                    className="rw-input"
-                    errorClassName="rw-input rw-input-error"
-                    ref={usernameRef}
-                    validation={{
-                      required: {
-                        value: true,
-                        message: 'Benutzertag muss angegeben sein!',
-                      },
-                    }}
-                  />
+              <div className="rw-segment-main">
+                <div className="rw-form-wrapper">
+                  <Form onSubmit={onSubmit} className="rw-form-wrapper">
+                    <Label
+                      name="username"
+                      className="rw-label"
+                      errorClassName="rw-label rw-label-error"
+                    >
+                      Benutzertag
+                    </Label>
+                    <TextField
+                      name="username"
+                      className="rw-input"
+                      errorClassName="rw-input rw-input-error"
+                      ref={usernameRef}
+                      validation={{
+                        required: {
+                          value: true,
+                          message: 'Benutzertag muss angegeben sein!',
+                        },
+                      }}
+                    />
 
-                  <FieldError name="username" className="rw-field-error" />
+                    <FieldError name="username" className="rw-field-error" />
 
-                  <Label
-                    name="password"
-                    className="rw-label"
-                    errorClassName="rw-label rw-label-error"
-                  >
-                    Passwort
-                  </Label>
-                  <PasswordField
-                    name="password"
-                    className="rw-input"
-                    errorClassName="rw-input rw-input-error"
-                    autoComplete="current-password"
-                    validation={{
-                      required: {
-                        value: true,
-                        message: 'Passwort muss angegeben sein!',
-                      },
-                    }}
-                  />
+                    <Label
+                      name="password"
+                      className="rw-label"
+                      errorClassName="rw-label rw-label-error"
+                    >
+                      Passwort
+                    </Label>
+                    <PasswordField
+                      name="password"
+                      className="rw-input"
+                      errorClassName="rw-input rw-input-error"
+                      autoComplete="current-password"
+                      validation={{
+                        required: {
+                          value: true,
+                          message: 'Passwort muss angegeben sein!',
+                        },
+                      }}
+                    />
 
-                  <div className="rw-forgot-link">
-                    {/* <Link
+                    <div className="rw-forgot-link">
+                      {/* <Link
                       to={routes.forgotPassword()}
                       className="rw-forgot-link"
                     >
                       Forgot Password?
                     </Link> */}
-                    Passwort vergessen? Melde dich bei deiner Leitung!
-                  </div>
+                      Passwort vergessen? Melde dich bei deiner Leitung!
+                    </div>
 
-                  <FieldError name="password" className="rw-field-error" />
+                    <FieldError name="password" className="rw-field-error" />
 
-                  <div className="rw-button-group">
-                    <Submit className="rw-button rw-button-blue">
-                      Einloggen
-                    </Submit>
-                  </div>
-                </Form>
+                    <div className="rw-button-group">
+                      <Submit className="rw-button rw-button-blue">
+                        Einloggen
+                      </Submit>
+                    </div>
+                  </Form>
+                </div>
               </div>
             </div>
-          </div>
+          ) : null}
           {/* <div className="rw-login-link">
             <span>Don&apos;t have an account?</span>{' '}
             <Link to={routes.signup()} className="rw-link">
               Sign up!
             </Link>
           </div> */}
-          {devmode ? <Devmode /> : null}
+          {devmode === 1 || devmode === 99 ? <Devmode mode={devmode} /> : null}
         </div>
 
       </main>
@@ -138,18 +144,46 @@ const LoginPage = () => {
   )
 }
 
-const Devmode = () => {
-  return (
-    <div className='rw-segment mt-2'>
-      <header className='rw-segment-header'>
-        <h2 className='rw-heading rw-heading-section'>
-          Account erstellen
-        </h2>
-      </header>
-      <div className='rw-segment-main'>
-        <button onClick={() => { navigate(routes.signup()) }}>Zur Signup-Page</button>
+const Devmode = ({ mode }) => {
+  const level1 = () => {
+    return (
+      <div className='rw-segment mt-2' >
+        <header className='rw-segment-header'>
+          <h2 className='rw-heading rw-heading-section'>
+            Account erstellen (Devmode)
+          </h2>
+        </header>
+        <div className='rw-segment-main'>
+          <button onClick={() => { navigate(routes.signup()) }}>Zur Signup-Page</button>
+        </div>
+      </div >
+    )
+  }
+  const level99 = () => {
+    return (
+      <div className='rw-segment mt-2'>
+        <header className='rw-segment-header'>
+          <h2 className='rw-heading rw-heading section'>
+            Was denkst du was du tust?
+          </h2>
+        </header>
+        <div className='rw-segment-main'>
+          Der Devmode. Du hast ihn also auch entdeckt! Nur zu schade, dass die "true" Option nicht existiert. <br />
+          So einfach mach ichs dir nun auch nicht. An den Devmode wirst du nicht rankommen. Du brauchst nämlich einen Token, der als Umgebungsvariable gesetzt wurde. <br />
+          Als Tipp: Der Token ist 16 Zeichen lang und besteht aus Ziffern und Buchstaben. <br />
+          Selbst wenn du ihn mit dem Tipp erraten konntest, muss ich dich enttäuschen. Der Devmode-Key wird jeden Tag neu generiert. Im Endeffekt bin ich der einzige, der längerfristig Zugriff auf ihn hat. <br />
+          <br />
+          Deine IP wurde geloggt. Ich wage mich nichtmal daran zu denken, was passieren würde, wenn deine IP mit einem User verknüpft wäre... <br />
+          <br />
+          Ich wünsche dir viel Spaß weiterhin.
+        </div>
       </div>
-    </div>
+    )
+  }
+  return (
+    <>
+      {mode === 1 ? level1() : mode === 99 ? level99() : null}
+    </>
   )
 }
 
