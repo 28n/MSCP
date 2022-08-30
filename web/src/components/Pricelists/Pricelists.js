2a1585c3252e0f506aedc658ab34228ca761385f
28n <28nh04@gmail.com>
2022-08-30 13:51:40 +0200

    changed pricelists to be displayed in correct order

--- web/src/components/Pricelists/Pricelists.js
+++ web/src/components/Pricelists/Pricelists.js
@@ -1,29 +1,88 @@
+import React from "react"
+import { useState, useEffect } from "react"
+import * as FaIcons from 'react-icons/fa'
+
 const Pricelists = ({ pricelists }) => {
+  let len = pricelists.length
+  let oddlists = []
+  let evenlists = []
+  for (let i = 0; i < len; i++) {
+    if (pricelists[i].id % 2) {
+      oddlists.push(pricelists[i])
+    } else {
+      evenlists.push(pricelists[i])
+    }
+  }
   return (
-    <div className="dashboard-content">
+    <>
+      <div className="dashboard-content">
+        <div className="col">
+          {oddlists.map((val) => {
+            const [open, setOpen] = useState(false)
+            const setContent = (content) => {
+              return { __html: content }
+            }
+            return (
+              <div className="infocard">
+                <div className="header">
+                  <span>{val.title}</span>
+                </div>
+                <div className="content">
+                  <div className={open ? "h-100 w-full flex" : "h-0 w-full hidden"} dangerouslySetInnerHTML={setContent(val.content)}>
+                  </div>
+                  <button className="mt-2" onClick={() => { setOpen(!open) }}>{open ? <FaIcons.FaChevronUp /> : <FaIcons.FaChevronDown />}</button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <div className="col">
+          {evenlists.map((val) => {
+            const [open, setOpen] = useState(false)
+            const setContent = (content) => {
+              return { __html: content }
+            }
+            return (
+              <div className="infocard">
+                <div className="header">
+                  <span>{val.title}</span>
+                </div>
+                <div className="content">
+                  <div className={open ? "h-100 w-full flex" : "h-0 w-full hidden"} dangerouslySetInnerHTML={setContent(val.content)}></div>
+                  <button className="mt-2" onClick={() => { setOpen(!open) }}>{open ? <FaIcons.FaChevronUp /> : <FaIcons.FaChevronDown />}</button>
+                </div>
+              </div>
+            )
+          })}
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Pricelists
+
+{/*
+
       <div className="grid grid-cols-2 gap-4 h-fit w-full">
-        {pricelists.map((val)=>{
+        {pricelists.map((val) => {
+          const [open, setOpen] = useState(false)
           const setContent = (content) => {
-            return {__html: content}
+            return { __html: content }
           }
           return (
             <div className="infocard">
               <div className="header">
                 <span>{val.title}</span>
               </div>
-                <div className="content" dangerouslySetInnerHTML={setContent(val.content)}></div>
+              <div className="content">
+                <div className={open ? "h-100 flex" : "h-0 hidden"} dangerouslySetInnerHTML={setContent(val.content)}></div>
+                <button className="mt-2" onClick={() => { setOpen(!open) }}>{open ? <FaIcons.FaChevronUp /> : <FaIcons.FaChevronDown />}</button>
+              </div>
             </div>
-          
-        )})}
-        <div className="infocard">
-          <div className="header">
-            <span>Test</span>
-          </div>
-          <div className="content">content</div>
-        </div>
+          )
+        })}
       </div>
-    </div>
-  )
-}
 
-export default Pricelists
+*/}
