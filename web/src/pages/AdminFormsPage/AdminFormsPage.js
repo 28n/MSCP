b4aa6ac5ad5597f387547dac3fb3d20fe370dfc9
28n <28nh04@gmail.com>
2022-08-30 13:52:44 +0200

    create debug iteration of admin-forms

--- web/src/pages/AdminFormsPage/AdminFormsPage.js
+++ web/src/pages/AdminFormsPage/AdminFormsPage.js
@@ -1,26 +1,19 @@
 import { MetaTags } from '@redwoodjs/web'
 import AdminVacationCell from 'src/components/AdminVacationCell/AdminVacationCell.js'
 import AdminTraineereviewCell from 'src/components/AdminTraineereviewCell/AdminTraineereviewCell.js'
+import AdminMeetingSignoffCell from 'src/components/AdminMeetingSignoffCell/AdminMeetingSignoffCell'
 
 const AdminFormsPage = () => {
   return (
     <>
       <MetaTags title="AdminForms" description="AdminForms page" />
 
-      <h1>Formulare verwalten</h1>
-      <h2>Hier siehst du nur das, was du sehen darfst. </h2>
-      <div className="c">
-        <p>content</p>
-        <p>content</p>
-        <p>content</p>
-        <p>content</p>
-      </div>
-      <div className="c">
-        <div className="grid grid-cols-2 gap-1 adminforms">
-          <AdminVacationCell />
-          <AdminTraineereviewCell />
-        </div>
-      </div>
+      <h1>Formularantworten</h1>
+      <h2>Hier findest du alle Kontaktaufnahmen chronologisch aufgelistet</h2>
+
+      <AdminVacationCell />
+      <AdminTraineereviewCell />
+      <AdminMeetingSignoffCell />
     </>
   )
 }
