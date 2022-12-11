42b0f13d34c94f8c4ad59c7a835910b77953e018
28n <28nh04@gmail.com>
2022-12-11 17:16:55 +0100

    taem elan

--- web/src/components/AdminVacation/AdminVacation.js
+++ web/src/components/AdminVacation/AdminVacation.js
@@ -1,8 +1,27 @@
 const AdminVacation = ({ vacationRequests }) => {
   return (
     vacationRequests.map((val)=>{
+      let date = new Date(val.createdAt)
+      let dateString = date.toLocaleString()
+      let startDate = new Date(val.startDate)
+      let endDate = new Date(val.endDate)
+      function getStatus(status) {
+        switch (status) {
+          case pending:
+            
+            break;
+
+          default:
+            break;
+        }
+      }
       return (
-        JSON.stringify(val)
+        <>
+          <h2 className="font-bold">{val.userName} hat am {dateString} Urlaub beantragt.</h2>
+          <p>Zeitraum: {startDate.toLocaleDateString()} - {endDate.toLocaleDateString()}</p>
+        <h2 className="font-bold">Status: </h2>
+          {JSON.stringify(val)}
+        </>
       )
     })
   )
